bf802b4bf0a3a331029abbb6bbbaff335435164c
Lenar <flfforwork@gmail.com>
2021-03-08 13:35:58 +0300

    feat: implement revers function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    let result = "";
+    for (let num of str) {
+        if (num === "-") continue;
+        result = num + result;
+    }
+    return +result;
+};
